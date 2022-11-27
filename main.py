6e1260de5d41a23dd5e3e7a4398f61f8499f1f50
qowngus33 <qowngus33@naver.com>
2022-11-27 15:32:11 +0900

    change LearnCodingWithTurtle parameter

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import tkinter as tk
-import time
 
 from game import GamePage
 from stage import Stage
 from imageFrame import ImageFrame
 
 class LearnCodingWithTurtle:
-    def __init__(self, root):
+    def __init__(self, root, instructList, goal):
         self.root = root
         self.root.title("LCWT")
         self.root.geometry("600x400")
         self.root.resizable(False, False)
 
-        self.gameFrame = [tk.Frame(self.root) for _ in range(len(instructList))]
-        self.game = [GamePage(self.gameFrame[i],instructList[i],goal[i]) for i in range(len(instructList))]
+        self.instructList = instructList
+        self.goal = goal
+
+        self.gameFrame = [tk.Frame(self.root) for _ in range(len(self.instructList))]
+        self.game = [GamePage(self.gameFrame[i],self.instructList[i],goal[i]) for i in range(len(self.instructList))]
         self.startFrame = ImageFrame(self.root,"image/startPage.png")
         self.gameTableFrame = ImageFrame(self.root,"image/gameTable.png")
 
         for i in range(len(self.gameFrame)):
             self.gameFrame[i].grid(row=0,column=0,sticky="nsew")
-            self.game[i].homeBtn.configure(text = "Home", command=lambda:[self.gameTableFrame.tkraise()])
+            self.game[i].homeBtn.configure(text="Home",command=lambda:[self.gameTableFrame.tkraise()])
 
-        self.gameTableFrame.grid(row=0, column=0, sticky="nsew")
-        self.startFrame.grid(row=0, column=0, sticky="nsew")
+        self.gameTableFrame.grid(row=0,column=0,sticky="nsew")
+        self.startFrame.grid(row=0,column=0,sticky="nsew")
 
         self.btnToTable = tk.Button(self.startFrame,
                                     width=15,
@@ -34,13 +36,13 @@ class LearnCodingWithTurtle:
         self.btnToTable.place(x=190,y=300)
 
         self.btnToGame = []
-        for i in range(len(instructList)):
+        for i in range(len(self.instructList)):
             self.btnToGame.append(tk.Button(self.gameTableFrame,
                                             text=str(i+1),
                                             bg='white',
                                             font=("Arial", 15),
                                             command=lambda c=i:self.start_game(self.gameFrame[c],self.game[c])))
-            self.btnToGame[i].place(x=95*(i+1), y=130*((i%2==1)+1))
+            self.btnToGame[i].place(x=95*(i+1),y=130*((i%2==1)+1))
         self.startFrame.tkraise()
 
     def start_game(self, frame, game):
@@ -49,6 +51,6 @@ class LearnCodingWithTurtle:
         game.show_goal()
 
 if __name__ == '__main__':
-    instructList, goal = Stage("stage/").stages()
-    app = LearnCodingWithTurtle(tk.Tk())
+    instruct_list, goals = Stage("stage/").stages()
+    app = LearnCodingWithTurtle(tk.Tk(),instruct_list,goals)
     tk.mainloop()
